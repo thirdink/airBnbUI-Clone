d0d5c74eadc856e784f9a6073186e2fb7ea8c168
Rumit Tuladhar <rumitwashere@gmail.com>
2019-01-27 11:54:57 +0545

    react navigation bottome tab and explore screen

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation';
 
-export default class App extends React.Component {
+import Icon from 'react-native-vector-icons/Ionicons';
+import Explore from './screens/Explore';
+import Saved from './screens/Saved';
+import Trips from './screens/Trips';
+import Inbox from './screens/Inbox';
+import Profile from './screens/Profile';
+
+ class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -10,6 +18,70 @@ export default class App extends React.Component {
     );
   }
 }
+export default createBottomTabNavigator({
+  Explore:{
+    screen: Explore,
+    navigationOptions:{
+      tabBarLabel:'EXPLORE',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-search" color={tintColor} size={24} />
+      )
+    }
+  },
+  Saved:{
+    screen:Saved,
+    navigationOptions:{
+      tabBarLabel:'SAVED',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-heart-empty" color={tintColor} size={24} />
+      )
+    }
+  },
+  Trips:{
+    screen:Trips,
+    navigationOptions:{
+      tabBarLabel:'TRIPS',
+      tabBarIcon:({tintColor})=>(
+        <Image source={require('./assets/airbnb.png')} 
+        style={{height:24,width:24,tintColor: tintColor}}
+        />
+
+      )
+    }
+  },
+  Inbox:{
+    screen:Inbox,
+    navigationOptions:{
+      tabBarLabel:'EXPLORE',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-chatboxes" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile:{
+    screen:Profile,
+    navigationOptions:{
+      tabBarLabel:'PROFILE',
+      tabBarIcon:({tintColor})=>(
+        <Icon name="ios-person" color={tintColor} size={24} />
+      )
+    }
+  }
+},{
+  tabBarOptions:{
+    activeTintColor:'red',
+    inactiveTintColor:'grey',
+    style:{
+      backgroundColor:'white',
+      borderTopWidth:0,
+      shadowOffset:{width:5,height:3},
+      shadowColor:'black',
+      shadowOpacity:0.5,
+      elevation: 5 // works on android but not modifyable
+      
+    }
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
