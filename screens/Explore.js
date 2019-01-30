b0bbae7dff449b4f5503cfb9dba21b71ac698601
Rumit Tuladhar <rumitwashere@gmail.com>
2019-01-30 12:45:55 +0545

    finished airBNB ui first page

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { 
+import {
     View,
     Text,
     StyleSheet,
@@ -9,93 +9,158 @@ import {
     StatusBar,
     ScrollView,
     Image,
-    Dimensions
+    Dimensions,
+    Animated
 } from "react-native";
-import Category from './components/explore/Category';
-import Icon from 'react-native-vector-icons/Ionicons';
-import Home from './components/explore/Home';
-
-const{height,width} = Dimensions.get('window');
+import Icon from 'react-native-vector-icons/Ionicons'
+import Category from './components/explore/Category'
+import Home from './components/explore/Home'
+import Tag from './components/explore/Tag'
+const { height, width } = Dimensions.get('window')
 class Explore extends Component {
-    componentWillMount(){
-        this.startHeaderHeight = 80
-        if(Platform.OS == 'android')
-        {
+
+    componentWillMount() {
+
+        this.scrollY = new Animated.Value(0)
+
+        this.startHeaderHeight = 90
+        this.endHeaderHeight = 50
+        if (Platform.OS == 'android') {
             this.startHeaderHeight = 100 + StatusBar.currentHeight
+            this.endHeaderHeight = 70 + StatusBar.currentHeight
         }
+
+        this.animatedHeaderHeight = this.scrollY.interpolate({
+            inputRange: [0, 50],
+            outputRange: [this.startHeaderHeight, this.endHeaderHeight],
+            extrapolate: 'clamp'
+        })
+
+        this.animatedOpacity = this.animatedHeaderHeight.interpolate({
+            inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+            outputRange: [0, 1],
+            extrapolate: 'clamp'
+        })
+        this.animatedTagTop = this.animatedHeaderHeight.interpolate({
+            inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+            outputRange: [-30, 10],
+            extrapolate: 'clamp'
+        })
+        this.animatedMarginTop = this.animatedHeaderHeight.interpolate({
+            inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+            outputRange: [50, 30],
+            extrapolate: 'clamp'
+        })
+
+
     }
 
     render() {
         return (
-            <SafeAreaView style={{flex:1}}>
-            <View style={{flex:1}}>
-                 <View style={{height: this.startHeaderHeight,backgroundColor:'white',borderBottomWidth:1,borderBottomColor:'#dddddd'}}>
-                    <View style={{flexDirection:'row',padding:10,backgroundColor:'white',marginHorizontal:20,
-                            shadowOffset:{width:0,height:0},
-                            shadowColor:'black',
-                            shadowOpacity:0.2,
-                            elevation:1,
-                            marginTop: Platform.OS == 'android' ? 30:null
-                            }}>
-                        <Icon name="ios-search" size={20} style={{marginRight:10}}/>
-                        <TextInput 
-                        underlineColorAndroid="transparent"
-                        placeholder="Try Kathmandu"
-                        placeholderTextColor="grey"
-                        style={{flex:1,fontWeight:'700',backgroundColor:'white'}}
-                        />
-                    </View>
-                 </View>
-                 <ScrollView
-                 showsVerticalScrollIndicator={false}
-                 scrollEventThrottle={16}
-                 >
-                 <View style={{flex:1, backgroundColor:'white',paddingTop:20}}>
-            <Text style={{fontSize:24,fontWeight:'700',paddingHorizontal:20}}>
-                 What can we help you find, Rumit?
-            </Text>
-            <View style={{height:130,marginTop:20}}>
-            <ScrollView horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            >
-                <Category imageUri={require('../assets/home.jpg')} 
-                     name="Home"
-                     />
-                <Category imageUri={require('../assets/experiences.jpg')} 
-                name="Experiences"
-                />
-                 <Category imageUri={require('../assets/restaurant.jpg')} 
-                name="Resturants"
-                />
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <Animated.View style={{ height: this.animatedHeaderHeight, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#dddddd' }}>
+                        <View style={{
+                            flexDirection: 'row', padding: 10,
+                            backgroundColor: 'white', marginHorizontal: 20,
+                            shadowOffset: { width: 0, height: 0 },
+                            shadowColor: 'black',
+                            shadowOpacity: 0.2,
+                            elevation: 1,
+                            marginTop: Platform.OS == 'android' ? 30 : null
+                        }}>
+                            <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+                            <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="Try Kathmandu"
+                                placeholderTextColor="grey"
+                                style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+                            />
+                        </View>
+                        <Animated.View
+                            style={{ flexDirection: 'row', marginHorizontal: 20, position: 'relative', top: this.animatedTagTop, opacity: this.animatedOpacity }}
+                        >
+                            <Tag tagName="Guests" />
+                            <Tag tagName="Dates" />
 
-            </ScrollView>
+                        </Animated.View>
+                    </Animated.View>
+                    <ScrollView
+                        scrollEventThrottle={16}
+                        onScroll={Animated.event(
+                            [
+                                { nativeEvent: { contentOffset: { y: this.scrollY } } }
+                            ]
+                        )}
+                    >
+                        <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }}>
+                            <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }}>
+                                What can we help you find, Rumit?
+                            </Text>
 
-            </View>
-            <View style={{marginTop:40,paddingHorizontal:20}}>
-                <Text style={{fontSize:24,fontWeight:'700'}}>Introducing Airbnb Plus</Text>
-                <Text style={{fontWeight:'100',marginTop:10}}>A new selection of homes verified for quality and comfort</Text>
-                <View style={{width:width-40,height:200,marginTop:20}}>
-                    <Image 
-                    style={{flex:1,height:null,width:null,resizeMode:'cover',borderRadius:5,borderWidth:1,borderColor:'#dddddd'}}
-                    source = {require('../assets/experiences.jpg')}
-                    />
+                            <View style={{ height: 130, marginTop: 20 }}>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    <Category imageUri={require('../assets/home.jpg')}
+                                        name="Home"
+                                    />
+                                    <Category imageUri={require('../assets/experiences.jpg')}
+                                        name="Experiences"
+                                    />
+                                    <Category imageUri={require('../assets/restaurant.jpg')}
+                                        name="Resturant"
+                                    />
+                                </ScrollView>
+                            </View>
+                            <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                                <Text style={{ fontSize: 24, fontWeight: '700' }}>
+                                    Introducing Airbnb Plus
+                                </Text>
+                                <Text style={{ fontWeight: '100', marginTop: 10 }}>
+                                    A new selection of homes verified for quality & comfort
+
+                                </Text>
+                                <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+                                    <Image
+                                        style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd' }}
+                                        source={require('../assets/home.jpg')}
+                                    />
+
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{ marginTop: 40 }}>
+                            <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }}>
+                                Homes around the world
+                            </Text>
+                            <View style={{ paddingHorizontal: 20, marginTop: 20, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                                <Home width={width}
+                                    name="The Cozy Place"
+                                    type="PRIVATE ROOM - 2 BEDS"
+                                    price={82}
+                                    rating={4}
+                                />
+                                <Home width={width}
+                                    name="The Cozy Place"
+                                    type="PRIVATE ROOM - 2 BEDS"
+                                    price={82}
+                                    rating={4}
+                                />
+                                <Home width={width}
+                                    name="The Cozy Place"
+                                    type="PRIVATE ROOM - 2 BEDS"
+                                    price={82}
+                                    rating={4}
+                                />
+
+
+                            </View>
+                        </View>
+                    </ScrollView>
+
                 </View>
-            </View>
-            <View style={{ marginTop: 40}}>
-            <Text style={{ fontSize:24,fontWeight:'700',paddingHorizontal:20}}>
-                Homes around the world
-            </Text>
-            <View style={{paddingHorizontal:20, margintop: 20,flexDirection:'row',flexWrap:'wrap',justifyContent:'space-between'}}>
-                <Home width={width} rating={4} price={82} type="PRIVATE ROOM - 2 BEDS" name="The Cozy Place"/>
-                <Home width={width} rating={4} price={82} type="PRIVATE ROOM - 2 BEDS" name="The Cozy Place"/>
-                <Home width={width} rating={4} price={82} type="PRIVATE ROOM - 2 BEDS" name="The Cozy Place"/>
-            </View>
-
-            </View>
-         </View>
-                 </ScrollView>
-            </View>
-               
             </SafeAreaView>
         );
     }
